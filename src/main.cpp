f792da5ef00f35c0525163d14b4a1c1d275f51f7
ltonnai_ <lysiane.tonnaire@gmail.com>
2026-01-06 13:16:16 +0100

    avancer leopard

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,88 @@
-#include "../include/Creature.h"
 #include <GL/glut.h>
+#include "../include/Leopard.h"
+#include "../include/Terrain.h"
 
-Creature creature;
-int lastTime = 0;
+// Global variables
+Leopard* g_leopard = nullptr;
+Terrain* g_terrain = nullptr;
+int g_lastTime = 0;
+bool g_keys[256] = {false};
+
+// ============ OpenGL Functions ============
+void renderText(float x, float y, const char* text) {
+    glRasterPos2f(x, y);
+    for (const char* c = text; *c != '\0'; c++) {
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, *c);
+    }
+}
 
 void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
-    
-    gluLookAt(0, 3, 8,   // Caméra
-              0, 0, 0,   // Regarde vers
-              0, 1, 0);  // Haut
-    
-    creature.draw();
-    
+
+    // Camera follows leopard
+    float camX = g_leopard->position.x;
+    float camY = g_leopard->position.y + 3.5f;
+    float camZ = g_leopard->position.z - 6.0f;
+
+    gluLookAt(
+        camX, camY, camZ,
+        g_leopard->position.x, g_leopard->position.y + 0.5f, g_leopard->position.z,
+        0, 1, 0
+    );
+
+    g_terrain->draw();
+    g_leopard->draw();
+
+    // Display controls
+    glMatrixMode(GL_PROJECTION);
+    glPushMatrix();
+    glLoadIdentity();
+    glOrtho(0, 800, 600, 0, -1, 1);
+    glMatrixMode(GL_MODELVIEW);
+    glPushMatrix();
+    glLoadIdentity();
+
+    glDisable(GL_DEPTH_TEST);
+    glColor3f(1, 1, 1);
+    renderText(10, 30, "W/A/S/D: Move | ESC: Exit");
+    glEnable(GL_DEPTH_TEST);
+
+    glPopMatrix();
+    glMatrixMode(GL_PROJECTION);
+    glPopMatrix();
+    glMatrixMode(GL_MODELVIEW);
+
     glutSwapBuffers();
 }
 
 void timer(int value) {
     int currentTime = glutGet(GLUT_ELAPSED_TIME);
-    float deltaTime = (currentTime - lastTime) / 1000.0f;
-    lastTime = currentTime;
-    
-    creature.update(deltaTime);
-    creature.setRotation(creature.getRotation() + deltaTime * 10.0f);
-    
+    float deltaTime = (currentTime - g_lastTime) / 1000.0f;
+    g_lastTime = currentTime;
+
+    if (deltaTime > 0.016f) deltaTime = 0.016f;  // Cap deltaTime
+
+    g_leopard->update(deltaTime, g_terrain);
     glutPostRedisplay();
     glutTimerFunc(16, timer, 0);
+}
+
+void reshape(int w, int h) {
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45, (float)w / h, 0.1f, 100.0f);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void keyboard(unsigned char key, int x, int y) {
+    if (key == 27) exit(0);  // ESC
+    g_keys[key] = true;
+}
+
+void keyboardUp(unsigned char key, int x, int y) {
+    g_keys[key] = false;
 }
 
 void initGL() {
@@ -34,33 +90,40 @@ void initGL() {
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
     glEnable(GL_COLOR_MATERIAL);
-    
-    GLfloat lightPos[] = { 5, 10, 5, 1 };
-    glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
-}
+    glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
 
-void reshape(int w, int h) {
-    glViewport(0, 0, w, h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(45, (float)w/h, 0.1, 100);
-    glMatrixMode(GL_MODELVIEW);
+    GLfloat lightPos[] = {10, 15, 10, 1};
+    GLfloat lightAmb[] = {0.3f, 0.3f, 0.3f, 1};
+    GLfloat lightDiff[] = {1, 1, 1, 1};
+
+    glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, lightAmb);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, lightDiff);
+
+    glClearColor(0.5f, 0.7f, 1.0f, 1.0f);
 }
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(800, 600);
-    glutCreateWindow("Creature Test");
-    
+    glutCreateWindow("Leopard Game");
+
     initGL();
-    
+
+    g_leopard = new Leopard();
+    g_terrain = new Terrain();
+
+    g_leopard->setKeyState(g_keys);
+
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
-    
-    lastTime = glutGet(GLUT_ELAPSED_TIME);
-    glutTimerFunc(0, timer, 0);
-    
+    glutKeyboardFunc(keyboard);
+    glutKeyboardUpFunc(keyboardUp);
+
+    g_lastTime = glutGet(GLUT_ELAPSED_TIME);
+    glutTimerFunc(16, timer, 0);
+
     glutMainLoop();
     return 0;
 }
